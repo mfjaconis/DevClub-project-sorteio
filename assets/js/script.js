f63ba32ec987dfad506afb905179902b463d0042
mfjaconis <matheusjaconis@gmail.com>
2023-09-14 22:16:17 -0300

    projeto com interação do javascript finalizada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,53 @@
-const buttonSorteio = document.querySelector(".btn-sorteio")
+const buttonSorteio = document.querySelector(".btn-sorteio");
+const buttonReset = document.querySelector(".btn-reset");
+const minInput = document.querySelector("#min-input");
+const maxInput = document.querySelector("#max-input");
+const informationValueSorteio = document.querySelector(".number-draw");
+
+minInput.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      draw();
+    }
+  });
+  
+  maxInput.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      draw();
+    }
+  });
 
 function draw() {
-    const firstInputValue = document.querySelector("#first-input").value
-    const secondInputValue = document.querySelector("#second-input").value
+  let min = minInput.value;
+  let max = maxInput.value;
 
-    console.log(firstInputValue, secondInputValue)
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  if (min > max) {
+    alert("O valor mínimo não pode ser superior ao valor máximo");
+  } else if (min === "" || max === "") {
+    alert("Os campos não podem estar vazio");
+  } else if (min === max) {
+    alert("Os campos não podem ser iguais");
+  } else if (min < 0 || max < 0) {
+    alert("Não é aceito número negativo");
+  } else {
+    const result = Math.floor(Math.random() * (max - min) + min);
+
+    informationValueSorteio.innerHTML = result;
+
+    minInput.value = "";
+    maxInput.value = "";
+
+    if (result % 2 === 0) {
+      informationValueSorteio.classList.add("text-preto");
+    } else {
+      informationValueSorteio.classList.remove("text-preto");
+      informationValueSorteio.classList.add("text-vermelho");
+    }
+  }
 }
 
-buttonSorteio.addEventListener('click', draw)
+
+buttonSorteio.addEventListener("click", draw);
+
